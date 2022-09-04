78a6bd3c80b2a8c43b44f171bd2a27ffd932b484
Max Witt <kirayatail@gmail.com>
2022-09-05 00:39:10 +0200

    ID check guard for updating clients

--- websocket.js
+++ websocket.js
@@ -1,4 +1,4 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
 const clientToSend = (c) => {
   return {
@@ -7,91 +7,122 @@ const clientToSend = (c) => {
     methods: c.methods,
     info: c.info,
     group: c.group,
-    hidden: c.hidden
+    hidden: c.hidden,
   };
-}
+};
 
-const clients = []
+const clients = [];
 
-const updateClient = (data)  => {
-  if (data.type === 'all') {
-    clients[clients.findIndex(c => c.id === data.id)] = {
-      ...clients[clients.findIndex(c => c.id === data.id)],
-      ...data.payload
+const updateClient = (data) => {
+  const foundIndex = clients.findIndex((c) => c.id === data.id);
+  if (foundIndex > -1) {
+    if (data.type === "all") {
+      clients[foundIndex] = {
+        ...clients[foundIndex],
+        ...data.payload,
+      };
+    } else {
+      // console.log(`Setting ${data.type} for client ${data.id}`);
+      clients[foundIndex][data.type] = data.payload;
     }
-  } else {
-    clients[clients.findIndex(c => c.id === data.id)][data.type] = data.payload
+    clients
+      .filter((c) => c.type === "controller")
+      .forEach((c) =>
+        c.sock.send(
+          JSON.stringify({
+            type: "info",
+            id: data.id,
+            payload: clientToSend(clients[foundIndex]),
+          })
+        )
+      );
   }
-  clients.filter(c => c.type === 'controller').forEach(c => c.sock.send(JSON.stringify({
-    type: 'info',
-    id: data.id,
-    payload: clientToSend(clients[clients.findIndex(c => c.id === data.id)])
-  })))
-}
+};
 
 module.exports = () => {
   const wss = new WebSocket.Server({ noServer: true });
 
-  wss.on('connection', (ws, req) =>  {
-    console.log('Connected to new socket')
+  wss.on("connection", (ws, req) => {
+    console.log("Connected to new socket");
     console.log(req.socket.address());
-    ws.on('message', (msg) => {
-      const data = JSON.parse(msg)
-      if (data.type === 'handshake') {
-        const id = clients.map(c => c.id).reduce((cand, c) => cand > c ? cand : c + 1, 0);
+    ws.on("message", (msg) => {
+      const data = JSON.parse(msg);
+      if (data.type === "handshake") {
+        const id = clients
+          .map((c) => c.id)
+          .reduce((cand, c) => (cand > c ? cand : c + 1), 0);
         console.log(`Handshake with ${data.payload} ${id}`);
         clients.push({
           type: data.payload,
           id,
-          sock: ws
+          sock: ws,
         });
-        ws.on('close', (ws) => {
+        ws.on("close", (ws) => {
           console.log(`client ${id} disconnected`);
-          clients.splice(clients.findIndex(c => c.id === id), 1);
-          clients.filter(c => c.type === 'controller').forEach(c => {
-            c.sock.send(JSON.stringify({
-              type: 'delete',
-              payload: id
-            }))
+          clients.splice(
+            clients.findIndex((c) => c.id === id),
+            1
+          );
+          clients
+            .filter((c) => c.type === "controller")
+            .forEach((c) => {
+              c.sock.send(
+                JSON.stringify({
+                  type: "delete",
+                  payload: id,
+                })
+              );
+            });
+        });
+        ws.send(
+          JSON.stringify({
+            type: "handshake",
+            id,
           })
-        })
-        ws.send(JSON.stringify({
-          type: 'handshake',
-          id
-        }));
-        if (data.payload === 'controller') {
-          clients.filter(c => c.type !== 'controller').forEach(c => {
-            ws.send(JSON.stringify({
-              type: 'create',
-              payload: clientToSend(c)
-            }));
-          })
+        );
+        if (data.payload === "controller") {
+          clients
+            .filter((c) => c.type !== "controller")
+            .forEach((c) => {
+              ws.send(
+                JSON.stringify({
+                  type: "create",
+                  payload: clientToSend(c),
+                })
+              );
+            });
         } else {
-          clients.filter(c => c.type === 'controller').forEach(c => {
-            c.sock.send(JSON.stringify({
-              type: 'create',
-              payload: {id, type: data.payload}
-            }));
-          })
+          clients
+            .filter((c) => c.type === "controller")
+            .forEach((c) => {
+              c.sock.send(
+                JSON.stringify({
+                  type: "create",
+                  payload: { id, type: data.payload },
+                })
+              );
+            });
         }
       }
-      if (['info', 'methods', 'group', 'hidden', 'all'].includes(data.type)) {
-        updateClient(data)
+      if (["info", "methods", "group", "hidden", "all"].includes(data.type)) {
+        updateClient(data);
       }
 
-      if (data.type === 'command') {
-        clients.find(c => c.id === data.id)?.sock.send(msg);
+      if (data.type === "command") {
+        clients.find((c) => c.id === data.id)?.sock.send(msg);
       }
     });
   });
 
   setInterval(() => {
-    clients.forEach(c => c.sock.send(JSON.stringify({
-      type: 'ping'
-    })));
-  }, 10000)
+    clients.forEach((c) =>
+      c.sock.send(
+        JSON.stringify({
+          type: "ping",
+        })
+      )
+    );
+  }, 10000);
 
   return wss;
-}
-
-
+};
