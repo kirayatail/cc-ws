fc2b6e4a08997ca8a948cae0ee3f66ae07dc5e5a
Max Witt <kirayatail@gmail.com>
2026-02-20 00:12:25 +0100

    add progress info type

--- cc-ws-front/src/components/widgets/info.component.jsx
+++ cc-ws-front/src/components/widgets/info.component.jsx
@@ -1,20 +1,42 @@
+function progress(props) {
+  const progressWidth =
+    ((props.value - props.min) / (props.max - props.min)) * 100;
+  const targetWidth =
+    ((props.target - props.min) / (props.max - props.min)) * 100;
+  return (
+    <div className="info-progress">
+      <div className="mask">
+        <div className="bar" style={{ width: `${progressWidth}%` }}></div>
+      </div>
+      {props.target !== undefined && (
+        <div
+          className="target"
+          style={{
+            left: `calc(${targetWidth}% - ${(6 * targetWidth) / 100}px)`,
+          }}
+        ></div>
+      )}
+    </div>
+  );
+}
 
-export default function Info({info}) {
+export default function Info({ info }) {
   const buildInfo = (i) => {
-    if (i.type === 'number') {
-      return (<div className="info-value">
-        {i.value.toFixed(2)}
-      </div>)
+    if (i.type === "number") {
+      return <div className="info-value">{i.value.toFixed(2)}</div>;
     }
-    if (i.type === 'warning') {
-      return <div className={`info-warning${i.value ? ' active' : ''}`}></div>
+    if (i.type === "warning") {
+      return <div className={`info-warning${i.value ? " active" : ""}`}></div>;
     }
-    return (<div className="info-value">{i.value}</div>)
-  }
+    if (i.type === "progress") {
+      return progress(i);
+    }
+    return <div className="info-value">{i.value}</div>;
+  };
   return (
     <div className="info-item">
-      <label>{info.key}</label> 
+      <label>{info.key}</label>
       {buildInfo(info)}
     </div>
-  )
+  );
 }
